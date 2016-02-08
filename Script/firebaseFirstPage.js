0532e9187d4e85b7d9d03b02a855fcb061ea3341
VishakhaBhope <vishakhabhope@gmail.com>
2016-02-08 12:38:57 +0530

    firebase_bootstrap_assignment

--- Script/firebaseFirstPage.js
+++ Script/firebaseFirstPage.js
@@ -1,14 +1,14 @@
 var myFirebaseRef = new Firebase("https://trial-demo.firebaseio.com/");
-myFirebaseRef.child("news1").on("value", function(snapshot) {
+myFirebaseRef.child("sub-news1").on("value", function(snapshot) {
     
 	document.getElementById("news-1").innerHTML=snapshot.val();
 });
-myFirebaseRef.child("news2").on("value", function(snapshot) {
+myFirebaseRef.child("sub-news2").on("value", function(snapshot) {
   document.getElementById("news-2").innerHTML=snapshot.val();
 });
-myFirebaseRef.child("news3").on("value", function(snapshot) {
+myFirebaseRef.child("sub-news3").on("value", function(snapshot) {
   document.getElementById("news-3").innerHTML=snapshot.val();
 });
-myFirebaseRef.child("news4").on("value", function(snapshot) {
+myFirebaseRef.child("sub-news4").on("value", function(snapshot) {
   document.getElementById("news-4").innerHTML=snapshot.val();
 });
